b3ff9b6592392665d56164f666889cff0a1166eb
Diego <diaceituno@gmail.com>
2018-04-23 20:34:32 +0200

    Âdding entities

--- src/core/MainController.java
+++ src/core/MainController.java
@@ -1,7 +1,9 @@
 package core;
 
+import com.unboundid.ldap.sdk.Attribute;
 import com.unboundid.ldap.sdk.LDAPException;
 import com.unboundid.ldap.sdk.LDAPSearchException;
+import com.unboundid.ldap.sdk.SearchResultEntry;
 
 import entities.Configuration;
 
@@ -14,17 +16,39 @@ public class MainController {
 			config.setLdapRoot("dc=miqr,dc=local");
 			config.setLDAPHost("miqr.dyndns.org");
 			config.setLDAPPort("389");
-			config.setLDAPUser("Susbadmin");
+			config.setLDAPUser("Subadmin");
 			config.setLDAPPass("#qwandum40!");
 			
 			config.setMySQLHost("localhost");
 			config.setMySQLPort("3306");
 			config.setMySQLDatabase("eval");
 			
+			LDAPControl ldap = new LDAPControl();
 			try {
-				DBControl dbControl = DBControl.getInstance();
-			} catch (ClassNotFoundException e) {
-				Logger.getInstance().logError(e);
+				ldap.connect();
+			} catch (LDAPException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+			try {
+				ldap.login();
+			} catch (LDAPException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				SearchResultEntry[] entries = ldap.searchOU(Configuration.getInstance().getLdapRoot());
+				for(SearchResultEntry entry : entries) {
+					
+					for(Attribute attribute: entry.getAttributes()) {
+						System.out.println(attribute.getName() + ": " + attribute.getValue());
+					}
+				}
+			} catch (LDAPSearchException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			ldap.disconnect();
 	}
 }
